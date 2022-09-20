53e48b50c4ee30444005eff4fc5b23c56bddeb89
SebastianSchmülling <sebastian@MacBook-Air-von-Sebastian.local>
2022-09-20 18:36:23 +0200

    new repository

---
+++ NeuroConverter.java
@@ -0,0 +1,30 @@
+package Assignments;
+import java.util.Scanner;
+//import java.math.*;
+public class NeuroConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your amount?:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        converter(amount);
+    }
+    public static void converter(double amount) {
+        int wholecoin = (int)(amount);
+        double cents = amount-wholecoin;
+
+        double calculate = Math.round(100*cents);  // necessary to switch it into an integer later
+
+        int coins49 = (int)(calculate)/49;
+        calculate = calculate - coins49*49;
+        int coins9 = (int)(calculate)/9;
+        calculate = calculate - coins9*9;
+        int coins1 = (int)(calculate)/1;
+
+        System.out.println(wholecoin);
+        System.out.println(coins49);
+        System.out.println(coins9);
+        System.out.println(coins1);
+    }
+}
+
---
+++ RPSLO.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class RPSLO {
+    // Rock = 0, Paper = 1, Scissors = 2, Lizzard = 3, Spock = 4
+    // Scissors cut Paper
+    // Paper covers Rock
+    // Rock crushes Lizard
+    // Lizard poisons Spock
+    // Spock smashes Scissors
+    // Scissors decapitate Lizard
+    // Lizard eats Paper
+    // Paper disproves Spock
+    // Spock vaporizes Rock
+    // Rock crushes Scissors
+   
+        public static void main(String[] args) {
+         System.out.println("Enter your play: R, P, S, L,O");
+         Scanner scanner = new Scanner(System.in);
+            // Write your code below
+           char users_play = scanner.next().charAt(0);
+           //System.out.println("> "+users_play);
+           if((users_play == 'r')||(users_play == 'p')||(users_play == 's')||(users_play == 'l')||(users_play == 'o')||(users_play == 'R')||(users_play == 'P')||(users_play == 'S')||(users_play == 'L')||(users_play == 'O'))
+          {
+                char computers_choice = computers_choice();
+                System.out.println("Computer play is "+computers_choice);
+                users_play = Character.toLowerCase(users_play); //preparing the input of the user for swtich case statement which can only work with non capital letters
+                char winner = 't';
+              switch(users_play)
+              {
+                     case ('s'):
+                      if(computers_choice=='p'||computers_choice=='P') winner = 'u';
+                          else if(computers_choice=='L') winner = 'u';
+                          else if(computers_choice=='O') winner = 'c';
+                          else if(computers_choice=='R') winner = 'c';
+                          else if(computers_choice=='S') winner = 't';
+                    break;
+                
+                 case ('p'):
+                     if(computers_choice=='R') winner = 'u';
+                          else if(computers_choice=='O') winner = 'u';
+                          else if(computers_choice=='S') winner = 'c';
+                          else if(computers_choice=='L') winner = 'c';
+                          else if(computers_choice=='P') winner = 't';
+                      break;
+                  case ('r'):
+                      if(computers_choice=='L') winner = 'u';
+                          else if(computers_choice=='S') winner = 'u';
+                          else if(computers_choice=='P') winner = 'c';
+                          else if(computers_choice=='O') winner = 'c';
+                          else if(computers_choice=='R') winner = 't';
+                      break;  
+                    case ('l'):
+                        if(computers_choice=='O') winner = 'u';
+                             else if(computers_choice=='P') winner = 'u';
+                             else if(computers_choice=='R') winner = 'c';
+                             else if(computers_choice=='S') winner = 'c';
+                             else if(computers_choice=='L') winner = 't';
+                       break;
+                    case ('o'):
+                        if(computers_choice=='S') winner = 'u';
+                                else if(computers_choice=='R') winner = 'u';
+                                else if(computers_choice=='P') winner = 'c';
+                                else if(computers_choice=='L') winner = 'c';
+                                else if(computers_choice=='O') winner = 't';
+                     break;       
+               }
+               if(winner=='c') System.out.println("Computer wins!");
+               else if(winner=='u')System.out.println("You win!");
+               else if(winner=='t')System.out.println("It is a tie!");
+         }  
+         else System.out.println("Invalid input (valid inputs: R, P, S, L, O)");
+        }// Write your code above
+    public static char computers_choice()
+    {
+
+        int choice_value = (int)(Math.random()*(4-0+1)+0);
+        char choice;
+        switch(choice_value)
+        {
+            case 0: 
+                choice = 'R';
+                break;
+            case 1:
+                choice = 'P';
+                break;
+            case 2:
+                choice = 'S';
+                break;
+            case 3:
+                choice = 'L';
+                break;
+            case 4:
+                choice = 'O';
+                break;
+            default: 
+                choice = 'e';
+                break;   
+        }
+        return choice;
+    }
+}
