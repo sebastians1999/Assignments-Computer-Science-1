3fc0850f679e929fa1f19d6928eaff954dddec03
SebastianSchmülling <sebastian@MacBook-Air-von-Sebastian.local>
2022-09-20 18:44:16 +0200

    Assignment 3

---
+++ CoolNumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+
+
+
+
+
+public class CoolNumbers {
+    public static void main(String[] args) {
+        //This is just to test your method
+    }
+    public static boolean isCool(int x, int n) {
+     return true;
+    }
+    public static int countCools(int n, int a, int b) {
+     return n;
+    }
+    
+}
