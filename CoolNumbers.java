e37d97279e57ca178bf9650036192247c94ddeff
SebastianSchmülling <sebastian@MacBook-Air-von-Sebastian.local>
2022-09-21 12:23:09 +0200

    Version 1.0

--- CoolNumbers.java
+++ CoolNumbers.java
@@ -8,13 +8,66 @@ import java.util.*;
 
 public class CoolNumbers {
     public static void main(String[] args) {
-        //This is just to test your method
+
+
+    int n,a,b;
+    Scanner input = new Scanner(System.in);  
+    System.out.println("Which number(positiv integer) should be tested?");
+    n = input.nextInt();
+    input.nextLine();
+
+    System.out.println("Between which Intervall should the program look for cool numbers?(a,b)\na:");
+    a = input.nextInt();
+    input.nextLine();
+    System.out.println("b:");
+    a = input.nextInt();
+    input.nextLine();
+
+
+/* 
+
     }
-    public static boolean isCool(int x, int n) {
-     return true;
+
+
+
+    public static boolean isCool(int x, int n) 
+    {
+        if(n%x!=0)
+        return false;
+            else 
+            return true;
     }
-    public static int countCools(int n, int a, int b) {
-     return n;
+
+
+    public static int countCools(int n, int a, int b)
+    {
+        int Coolnumbers = 0;
+        if(a<b)
+        {
+            while(a<=b)
+            {
+                if(isCool(a,n)==true)
+                {
+                    Coolnumbers++;
+                }
+                a++;
+            }
+        }
+        else if(b<a)
+        {
+            while(a<=b)
+            {
+                if(isCool(b,n)==true)
+                {
+                    Coolnumbers++;
+                }
+                b++;
+            }
+        }
+        return Coolnumbers;
+        
+
     }
-    
+  */  
 }
+
