36feae40fdfdd579d0afe210c773da6db1d9a6ce
Sebastian Schmülling <sebastian.schmuelling@gmail.com>
2022-10-17 17:29:19 +0200

    finished 6. Assignment

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,6 +1,9 @@
 
 
 import java.math.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ArrayAssignment{
 
@@ -18,13 +21,62 @@ public class ArrayAssignment{
                               
 
 
-
+    int Array[] = {0,2,0,2,0,2,2,1,1,0};
     
-    diagonalPrint(Matrix);
+    //diagonalPrint(Matrix);
+    System.out.println(allNumsWithin(Array, 3));
+
+
     }
 
     public static int allNumsWithin(int[] A, int k) {
-        return 0;
+       
+        //two base cases
+        //would be the smallest possible AllNumsWithin List
+        if(A.length<k){
+            return A.length;
+        }   
+        
+        //checking whether all numbers are there
+        boolean[] check = new boolean[k];
+        for(int a = 0; a < A.length;a++){
+            check[A[a]]=true;
+        }
+
+        
+
+        //if not we return 0
+        for( int a = 0; a < check.length; a++){
+            if(check[a]==false){
+                return 0;
+            }
+        }
+        
+        //creating new arrays with new boundaries
+        int[] TestLeftSideArray = new int[A.length-1];
+        int[] TestRightSideArray = new int[A.length-1];
+        //copying arrays 
+       System.arraycopy(A,0, TestLeftSideArray, 0, A.length-1);
+       System.arraycopy(A, 1, TestRightSideArray, 0, A.length-1);
+        
+
+        //recursion
+        int l = allNumsWithin(TestLeftSideArray, k);
+        int r = allNumsWithin(TestRightSideArray, k);
+
+        if(l==0 && r==0){
+            return A.length;
+        }
+        else if(l== 0){
+            return r;
+        }
+        else if (r== 0){
+            return l;
+        }
+        //needed since one side could be smaller
+        else{
+            return Math.min(l,r);
+        }
     }
 
 
